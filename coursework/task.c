4bb564c5d1f054b0566760fb2263ac4c0e347eeb
Nadia Rahman <146254492+Nadia8844@users.noreply.github.com>
2023-10-30 18:47:33 +0000

    coursework

---
+++ coursework/task.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_RECORDS 100
+
+// Define an appropriate struct
+typedef struct {
+    char date[11];
+    char time[6];
+    int steps;
+} FITNESS_DATA;
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Outputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {
+        strcpy(date, token);
+    }
+
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+
+    // Free the duplicated string
+    free(inputCopy);
+}
+
+// Complete the main function
+int main() {
+    char filename[] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Error opening the file\n");
+        return 1;
+    }
+
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+    FITNESS_DATA fitnessDataArray[MAX_RECORDS];
+    int recordCount = 0;
+
+    while (fgets(line_buffer, buffer_size, file) != NULL && recordCount < MAX_RECORDS) {
+        char date[11], time[6], steps[10];
+        tokeniseRecord(line_buffer, ",", date, time, steps);
+        strcpy(fitnessDataArray[recordCount].date, date);
+        strcpy(fitnessDataArray[recordCount].time, time);
+        fitnessDataArray[recordCount].steps = atoi(steps);
+        recordCount++;
+    }
+
+    // Printing the stored data
+    for (int i = 0; i < recordCount; i++) {
+        printf("Date: %s, Time: %s, Steps: %d\n", fitnessDataArray[i].date, fitnessDataArray[i].time, fitnessDataArray[i].steps);
+    }
+
+    fclose(file);
+    return 0;
+}
