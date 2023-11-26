f6b58b9a24fd21bb235dd5968145b0cd18cd858b
Nadia Rahman <146254492+Nadia8844@users.noreply.github.com>
2023-11-26 15:58:48 +0000

    coursework2

--- coursework2/StepCounter_Final.c
+++ coursework2/StepCounter_Final.c
@@ -73,7 +73,6 @@ int main()
 
     
         choice = getchar();
-
         while (getchar() != '\n');
 
         switch (choice)
@@ -84,53 +83,121 @@ int main()
             counter = 0;
             while (fgets(line, buffer_size, input))
             {
-
                 tokeniseRecord(line, ",", fitnessdata[counter].date, fitnessdata[counter].time, &fitnessdata[counter].steps);
                 counter++;
             }
-            for (int i = 0; i < counter; i++)
-            {
-                printf("%s/%s - Steps: %d\n", fitnessdata[i].date, fitnessdata[i].time, fitnessdata[i].steps);
-            }
             fclose(input);
+            printf("File successfully loaded.\n");
             break;
-
+            
         case 'B':
         case 'b':
+           
             printf("Total number of records: %d\n", counter);
             fclose(input);
             break;
 
         case 'C':
         case 'c':
-          
+        
+            if (counter == 0) {
+                printf("No data available. Load data first (Option A).\n");
+            } else {
+                FITNESS_DATA fewestStepsSlot = fitnessdata[0];
 
-            break;
+                // Find the timeslot with the fewest steps
+                for (int i = 1; i < counter; i++) {
+                    if (fitnessdata[i].steps < fewestStepsSlot.steps) {
+                        fewestStepsSlot = fitnessdata[i];
+                    }
+                }
+
+                // Display the timeslot with the fewest steps
+                printf("Fewest steps: %s %s\n", fewestStepsSlot.date, fewestStepsSlot.time);
+            }
+                break;
 
         case 'D':
         case 'd':
+            if (counter == 0) {
+                printf("No data available. Load data first (Option A).\n");
+            } else {
+                FITNESS_DATA largestStepsSlot = fitnessdata[0];
 
+                // Find the timeslot with the largest number of steps
+                for (int i = 1; i < counter; i++) {
+                    if (fitnessdata[i].steps > largestStepsSlot.steps) {
+                        largestStepsSlot = fitnessdata[i];
+                    }
+                }
+
+                // Display the timeslot with the largest number of steps (without the steps value)
+                printf("Largest steps: %s %s\n", largestStepsSlot.date, largestStepsSlot.time);
+            }
             break;
 
         case 'E':
         case 'e':
+            if (counter == 0) {
+                printf("No data available. Load data first (Option A).\n");
+            } else {
+                int totalSteps = 0;
 
+                for (int i = 0; i < counter; i++) {
+                    totalSteps += fitnessdata[i].steps;
+                }
+
+                float meanSteps = (float)totalSteps / counter;
+
+                // Display the mean step count
+                printf("Mean step count: %.0f\n", meanSteps);
+            }
             break;
 
         case 'F':
         case 'f':
-         
+            if (counter == 0) {
+                printf("No data available. Load data first (Option A).\n");
+            } else {
+                int currentlength = 0;
+                int maxlength = 0;
+                int startIdx = 0;
+
+                // Iterate through the fitnessdata array
+                for (int i = 0; i < counter; i++) {
+                    if (fitnessdata[i].steps > 500) {
+                        currentlength++;
+                    } else {
+                        // Check if the current period is longer than the maximum
+                        if (currentlength > maxlength) {
+                            maxlength = currentlength;
+                            startIdx = i - maxlength;
+                        }
+
+                        currentlength = 0;
+                    }
+                }
+
+                if (currentlength > maxlength) {
+                    startIdx = counter - maxlength;
+                }
+
+                // Display the longest continuous period above 500 steps
+                printf("Longest period start: %s %s\n", fitnessdata[startIdx].date, fitnessdata[startIdx].time);
+                printf("Longest period end: %s %s\n", fitnessdata[startIdx + maxlength - 1].date, fitnessdata[startIdx + maxlength - 1].time);
+            }
             break;
+
 
         case 'Q':
         case 'q':
             return 0;
 
-        // if they type anything else:
         default:
             printf("Invalid choice\n");
             break;
         }
     }
 }
+
 
