bb8ebe3ecefac5fda9437b60e9bea33f2609a3ba
Nadia Rahman <146254492+Nadia8844@users.noreply.github.com>
2023-12-09 21:35:41 +0000

    coursework3

--- coursework3/FitnessDataSorter.c
+++ coursework3/FitnessDataSorter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h> // Include the ctype.h header
 
 // Define the struct for the fitness data
 typedef struct {
@@ -19,6 +20,13 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
             strcpy(time, ptr);
             ptr = strtok(NULL, &delimiter);
             if (ptr != NULL) {
+                // Check for non-numeric characters in steps
+                for (int i = 0; ptr[i] != '\0'; i++) {
+                    if (!isdigit((unsigned char)ptr[i])) { // Ensure that isdigit is applied to an unsigned char
+                        fprintf(stderr, "Error: invalid file (non-numeric steps)\n");
+                        exit(1);
+                    }
+                }
                 *steps = atoi(ptr);
             } else {
                 fprintf(stderr, "Error: invalid file (missing steps)\n");
