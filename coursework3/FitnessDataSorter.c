70ae1f483ee2efc2505aa609b633a09c011f35a7
Nadia Rahman <146254492+Nadia8844@users.noreply.github.com>
2023-12-12 18:53:34 +0000

    coursework3

--- coursework3/FitnessDataSorter.c
+++ coursework3/FitnessDataSorter.c
@@ -1,102 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h> // Include the ctype.h header
+#include <stdbool.h>
+#include <ctype.h>
 
 // Define the struct for the fitness data
 typedef struct {
     char date[11];
     char time[6];
     int steps;
-} FITNESS_DATA;
+} FitnessData;
 
 // Function to tokenize a record
 void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
-    char *ptr = strtok(record, ",");
-
+    char *ptr = strtok(record, &delimiter);
     if (ptr != NULL) {
         strcpy(date, ptr);
-        ptr = strtok(NULL, ",");
+        ptr = strtok(NULL, &delimiter);
         if (ptr != NULL) {
             strcpy(time, ptr);
-            ptr = strtok(NULL, ",");
+            ptr = strtok(NULL, &delimiter);
             if (ptr != NULL) {
-                // Check for non-numeric characters in steps
-                for (int i = 0; ptr[i] != '\0'; i++) {
-                    if (!isdigit((unsigned char)ptr[i])) {
-                        fprintf(stderr, "Error: invalid file (non-numeric steps)\n");
-                        exit(1);
-                    }
-                }
                 *steps = atoi(ptr);
-            } else {
-                fprintf(stderr, "Error: invalid file (missing steps)\n");
-                exit(1);
             }
-        } else {
-            fprintf(stderr, "Error: invalid file (missing time)\n");
-            exit(1);
         }
-    } else {
-        fprintf(stderr, "Error: invalid file (missing date)\n");
-        exit(1);
     }
 }
 
+bool isValidRecord(char *record, char delimiter) {
+    char date[11], time[6];
+    int steps;
+    char *tmp = strdup(record);  // Create a temporary copy of record for validation
 
-// Function to compare two FITNESS_DATA records for sorting
-int compareRecords(const void *a, const void *b) {
-    return ((FITNESS_DATA *)b)->steps - ((FITNESS_DATA *)a)->steps;
+    tokeniseRecord(tmp, delimiter, date, time, &steps);
+    free(tmp);  // Free the temporary copy
+
+    // Check if any field is empty or if steps are not valid (atoi returns 0 for invalid conversion)
+    if (date[0] == '\0' || time[0] == '\0' || steps == 0) {
+        return false;
+    }
+
+    return true;
+}
+
+// Function to compare fitness data (for sorting)
+int compareFitnessData(const void *a, const void *b) {
+    FitnessData *dataA = (FitnessData *)a;
+    FitnessData *dataB = (FitnessData *)b;
+    return dataB->steps - dataA->steps;
 }
 
 int main() {
     char filename[256];
-
-    // Step 1: Ask the user for a filename
-    printf("Enter Filename: ");
+    printf("Enter filename: ");
     scanf("%s", filename);
 
     FILE *file = fopen(filename, "r");
-
-    // Step 1: Ensure the filename is valid and the file can be opened
-    if (file == NULL) {
-        fprintf(stderr, "Error: invalid file\n");
+    if (!file) {
+        printf("Error: invalid file\n");
         return 1;
     }
 
-    // Read the file into an array
-    FITNESS_DATA fitnessRecords[100]; // Adjust the size as needed
-    int recordCount = 0;
-    char line[100]; // Adjust the size as needed
+    FitnessData data[1000];
+    int count = 0;
+    char line[256];
 
-    while (fgets(line, sizeof(line), file) != NULL && recordCount < 100) {
-        // Use the tokeniseRecord function
-        tokeniseRecord(line, ',', fitnessRecords[recordCount].date, fitnessRecords[recordCount].time, &fitnessRecords[recordCount].steps);
-        recordCount++;
+    while (fgets(line, sizeof(line), file)) {
+        if (!isValidRecord(line, ',')) {
+            printf("Error: invalid file (non-numeric steps)\n");
+            fclose(file);
+            return 1;
+        }
+
+        tokeniseRecord(line, ',', data[count].date, data[count].time, &data[count].steps);
+        count++;
     }
-
     fclose(file);
 
-    // Step 2: Sort the data in descending order of steps
-    qsort(fitnessRecords, recordCount, sizeof(FITNESS_DATA), compareRecords);
+    // Sort the data
+    qsort(data, count, sizeof(FitnessData), compareFitnessData);
 
-    // Step 3: Write out the sorted data to a new file with .tsv extension
-    strcat(filename, ".tsv");
-    FILE *outputFile = fopen(filename, "w");
+    // Create output file name
+    char outputFilename[260];
+    snprintf(outputFilename, sizeof(outputFilename), "%s.tsv", filename);
 
-    if (outputFile == NULL) {
-        fprintf(stderr, "Error creating output file\n");
-        return 1;
+    // Write sorted data to new file
+    FILE *outputFile = fopen(outputFilename, "w");
+    for (int i = 0; i < count; i++) {
+        fprintf(outputFile, "%s\t%s\t%d\n", data[i].date, data[i].time, data[i].steps);
     }
-
-    for (int i = 0; i < recordCount; i++) {
-        fprintf(outputFile, "%s\t%s\t%d\n", fitnessRecords[i].date, fitnessRecords[i].time, fitnessRecords[i].steps);
-    }
-
     fclose(outputFile);
 
-    printf("Data sorted and written to %s\n", filename);
+    printf("Data sorted and written to %s\n", outputFilename);
 
-    // Successful execution
     return 0;
 }
+
+
+
